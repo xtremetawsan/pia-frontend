ae0bf0d3db3c13c99d745f2735806531154dbda5
xtremetawsan <ahmetkarakilcik@gmail.com>
2025-06-26 08:34:33 +0300

    next test app

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import styles from "./header.module.css";
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <div className="logo">Firma logosu....</div>
+    </div>
+  );
+}
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,3 @@
+export default function Sidebar() {
+  return <div className="sideBar">Sidebar...</div>;
+}
